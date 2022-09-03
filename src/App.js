fb8398a5b7abfc2594cb68016b60f241d089f179
gnsia <gnsia@naver.com>
2022-09-04 08:58:34 +0900

    add Photos component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
+import Photos from './components/Photos'
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
@@ -12,20 +12,7 @@ function App() {
         <br/>
       </header>
       <section>
-        <img src="https://i.ibb.co/xMGngCW/demo07.png" alt="demo07" border="5" className="image"/>
-        <img src="https://i.ibb.co/KKcZcmC/demo08.png" alt="demo08" border="5" className="image"/>
-        <img src="https://i.ibb.co/6nZjRc3/demo13.png" alt="demo13" border="5" className="image"/>
-        <img src="https://i.ibb.co/qdXZwCD/demo12.png" alt="demo12" border="5" className="image"/>
-        <img src="https://i.ibb.co/qdXZwCD/demo12.png" alt="demo12" border="5" className="image"/>
-        <img src="https://i.ibb.co/85hhHnd/demo11.png" alt="demo11" border="5" className="image"/>
-        <img src="https://i.ibb.co/VpC63wt/demo10.png" alt="demo10" border="5" className="image"/>
-        <img src="https://i.ibb.co/yVTwRbY/demo09.png" alt="demo09" border="5" className="image"/>
-        <img src="https://i.ibb.co/r6SvT2k/demo06.png" alt="demo06" border="5" className="image"/>
-        <img src="https://i.ibb.co/YNvZSP3/demo05.png" alt="demo05" border="5" className="image"/>
-        <img src="https://i.ibb.co/s2CLbxx/demo04.png" alt="demo04" border="5" className="image"/>
-        <img src="https://i.ibb.co/f0hH0KF/demo03.png" alt="demo03" border="5" className="image"/>
-        <img src="https://i.ibb.co/8ckCbZJ/demo01.png" alt="demo01" border="5" className="image"/>
-        <img src="https://i.ibb.co/y0CjZyz/demo02.png" alt="demo02" border="5" className="image"/>
+        <Photos />
       </section>
       <footer>
         <h2>Contact</h2>
