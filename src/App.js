aefacbd36cded98ed4c55812f8d1958a67a6df59
gnsia <gnsia@naver.com>
2022-09-03 18:45:33 +0900

    add image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <br/>
+        <h1>
+          Jinny Park Photography
+        </h1>
+        <br/>
       </header>
+      <section>
+        <img src="https://i.ibb.co/xMGngCW/demo07.png" alt="demo07" border="5" className="image"/>
+        <img src="https://i.ibb.co/KKcZcmC/demo08.png" alt="demo08" border="5" className="image"/>
+        <img src="https://i.ibb.co/6nZjRc3/demo13.png" alt="demo13" border="5" className="image"/>
+        <img src="https://i.ibb.co/qdXZwCD/demo12.png" alt="demo12" border="5" className="image"/>
+        <img src="https://i.ibb.co/qdXZwCD/demo12.png" alt="demo12" border="5" className="image"/>
+        <img src="https://i.ibb.co/85hhHnd/demo11.png" alt="demo11" border="5" className="image"/>
+        <img src="https://i.ibb.co/VpC63wt/demo10.png" alt="demo10" border="5" className="image"/>
+        <img src="https://i.ibb.co/yVTwRbY/demo09.png" alt="demo09" border="5" className="image"/>
+        <img src="https://i.ibb.co/r6SvT2k/demo06.png" alt="demo06" border="5" className="image"/>
+        <img src="https://i.ibb.co/YNvZSP3/demo05.png" alt="demo05" border="5" className="image"/>
+        <img src="https://i.ibb.co/s2CLbxx/demo04.png" alt="demo04" border="5" className="image"/>
+        <img src="https://i.ibb.co/f0hH0KF/demo03.png" alt="demo03" border="5" className="image"/>
+        <img src="https://i.ibb.co/8ckCbZJ/demo01.png" alt="demo01" border="5" className="image"/>
+        <img src="https://i.ibb.co/y0CjZyz/demo02.png" alt="demo02" border="5" className="image"/>
+      </section>
+      <footer>
+        <h2>Contact</h2>
+        <h4>jinnyparkphotography@gmail.com</h4>
+        <br/>
+        <br/>
+      </footer>
     </div>
   );
 }
